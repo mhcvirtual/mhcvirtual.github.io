72412c852e1a6880eea1d618ea79242869ebdb8a
mhcvirtual <mhcg.digital@gmail.com>
2020-03-24 20:00:09 -0300

    Arquivos não mais necessários deletados, edição do index.html - incluir nome do autor

--- js/index2.js
+++
@@ -1,8 +0,0 @@
-import Cursor from './cursor2';
-
-const cursor = new Cursor(document.querySelector('.cursor'));
-
-[...document.querySelectorAll('a')].forEach(el => {
-    el.addEventListener('mouseenter', () => cursor.emit('enter'));
-    el.addEventListener('mouseleave', () => cursor.emit('leave'));
-});
